c1a3095063ffcbac53722ded47a337d676e9546c
Frca <killerfrca@gmail.com>
2013-08-07 00:35:31 +0200

    Proper synchronization for Networking runnable
    Signed-off-by: Frca <killerfrca@gmail.com>

--- Purtges-endpoints/src/main/java/com/frca/purtges/requests/NetworkRunnable.java
+++ Purtges-endpoints/src/main/java/com/frca/purtges/requests/NetworkRunnable.java
@@ -3,6 +3,7 @@ package com.frca.purtges.requests;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -11,15 +12,13 @@ import java.util.concurrent.Future;
 
 public class NetworkRunnable implements Runnable {
 
-    private List<NetworkTask> tasks = new ArrayList<NetworkTask>();
+    private List<NetworkTask> tasks = Collections.synchronizedList(new ArrayList<NetworkTask>());
 
     private final long SLEEP_INTERVAL = 500;
     private final long CALLBACK_SLEEP_INTERVAL = 50;
 
     @Override
     public void run() {
-        Iterator<NetworkTask> itr;
-
         while(true) {
             if (tasks.isEmpty()) {
                 try {
@@ -32,56 +31,57 @@ public class NetworkRunnable implements Runnable {
 
             final List<Future<NetworkTask>> futures = new ArrayList<Future<NetworkTask>>();
 
-            itr = tasks.iterator();
+            synchronized (tasks) {
+                Iterator<NetworkTask> itr = tasks.iterator();
 
-            // add tasks
-            while (itr.hasNext()) {
-                final NetworkTask task = itr.next();
-                itr.remove();
-                futures.add(
-                    Executors.newSingleThreadExecutor().submit(new Callable<NetworkTask>() {
-                        @Override
-                        public NetworkTask call() throws Exception {
-                            Log.d("TASK_MGMNT", "Executing task `" + task.getTaskName() + "` from queue.");
-                            return task.execute();
-                        }
-                    }));
-            }
+                // add tasks
+                while (itr.hasNext()) {
+                    final NetworkTask task = itr.next();
+                    itr.remove();
+                    futures.add(
+                        Executors.newSingleThreadExecutor().submit(new Callable<NetworkTask>() {
+                            @Override
+                            public NetworkTask call() throws Exception {
+                                Log.d("TASK_MGMNT", "Executing task `" + task.getTaskName() + "` from queue.");
+                                return task.execute();
+                            }
+                        }));
+                }
 
-            // call callbacks
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    Iterator<Future<NetworkTask>> callbackItr;
+                // call callbacks
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Iterator<Future<NetworkTask>> callbackItr;
 
-                    do {
-                        callbackItr = futures.iterator();
+                        do {
+                            callbackItr = futures.iterator();
 
-                        while (callbackItr.hasNext()) {
-                            Future<NetworkTask> future = callbackItr.next();
-                            if (future.isDone() || future.isCancelled()) {
-                                callbackItr.remove();
-                                try {
-                                    if (future.isDone()) {
-                                        Log.d("TASK_MGMNT", "Callbacking task `" + future.get().getTaskName() + "` from queue.");
-                                        future.get().callback();
+                            while (callbackItr.hasNext()) {
+                                Future<NetworkTask> future = callbackItr.next();
+                                if (future.isDone() || future.isCancelled()) {
+                                    callbackItr.remove();
+                                    try {
+                                        if (future.isDone()) {
+                                            Log.d("TASK_MGMNT", "Callbacking task `" + future.get().getTaskName() + "` from queue.");
+                                            future.get().callback();
+                                        }
+                                    } catch (Exception e) {
+                                        Log.e(e.getClass().getName(), e.getMessage() != null ? e.getMessage() : "<empty>");
                                     }
-                                } catch (Exception e) {
-                                    Log.e(e.getClass().getName(), e.getMessage() != null ? e.getMessage() : "<empty>");
                                 }
                             }
-                        }
 
-                        try {
-                            Thread.sleep(CALLBACK_SLEEP_INTERVAL);
-                        } catch (InterruptedException e) {
-                            return;
-                        }
+                            try {
+                                Thread.sleep(CALLBACK_SLEEP_INTERVAL);
+                            } catch (InterruptedException e) {
+                                return;
+                            }
 
-                    } while(!futures.isEmpty());
-                }
-            }).start();
-
+                        } while(!futures.isEmpty());
+                    }
+                }).start();
+            }
         }
     }
 
